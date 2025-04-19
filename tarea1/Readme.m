4a7725e7ea24d95b60e641e9b43efaf3a74d4521
vadiazg <vadiazg@udistrital.edu.co>
2025-04-19 12:54:00 -0500

    subiendo1

--- tarea1/Readme.m
+++ tarea1/Readme.m
@@ -1,4 +1,37 @@
+
 #Rimas 
-  ## Encontrar las palabras que terminan las letras dadas
+
+  ##Encontrar las palabras que terminan las letras dadas
 
    ##A partir de lo anterior realice una aplicación (sorpréndame)
+
+_Procesos desarrollados_ 
+
+*Se generan  frases nuevas con combinaciones aleatorias.
+
+*Se organiza los datos en una tabla estructurada con Pandas.
+
+*Se muestra un gráfico de barras con la cantidad de frases por rima.
+
+*Unir las frases: Junta todas las frases en un solo texto para que las palabras se puedan analizar en conjunto.
+
+*Generar la nube de palabras: Crea una visualización donde las palabras más frecuentes se muestran más grandes.
+
+*Visualización: Muestra la nube de palabras en una ventana con un fondo oscuro y colores fríos, lo que le da un toque visual chido.
+
+Después de correr este código, verás una imagen donde las palabras que más se repiten en tus frases se muestran más grandes, 
+y puedes visualizar rápidamente qué términos son los más importantes en el estilo "rap" que creamos.
+
+*Obtención de sinónimos: Usamos nltk y su módulo wordnet para obtener sinónimos de cada palabra en las frases originales.
+
+*Reemplazo de palabras: Reemplazamos cada palabra en las frases por uno de sus sinónimos de manera aleatoria, creando nuevas versiones de las frases.
+
+
+Construir el grafo : Creamos un grafo donde cada frase es un nodo y las frases que comparten la misma rima están
+ conectadas por una arista (línea).
+
+  *Utilizamos matplotlib para visualizar cómo se conectan las frases de acuerdo con las rimas.
+
+  +Obtención de sinónimos: Usamos nltk y su módulo wordnet para obtener sinónimos de cada palabra en las frases originales.
+
+*Reemplazo de palabras: Reemplazamos cada palabra en las frases por uno de sus sinónimos de manera aleatoria, creando nuevas versiones de las frases.
